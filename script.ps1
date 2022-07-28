6110f3a3e3247ec6cbb805b8afe91bc45f112ac4
Daniel Wator <daniel_wator@epam.com>
2022-07-28 16:56:59 +0200

    fix uris

--- script.ps1
+++ script.ps1
@@ -16,10 +16,10 @@ Get-Random -InputObject $NameList
 New-AzResourceGroup -Name $rgname -Location $location
 Write-Host "Press any key to continue..."
 
-New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/keyvault.json -TemplateParameterUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/keyvault.parameters.json
+New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/keyvault.template.json -TemplateParameterUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/keyvault.parameters.json
 Write-Host "Press any key to continue..."
 
-New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/webapp.json -TemplateParameterUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/webapp.parameters.json
+New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/webapp.template.json -TemplateParameterUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/webapp.parameters.json
 Write-Host "Press any key to continue..."
 
-New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/insights.json -TemplateParameterUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/insights.parameters.json
+New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/insights.template.json -TemplateParameterUri https://raw.githubusercontent.com/ddeer1109/iaas-exercises/main/insights.parameters.json
